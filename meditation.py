d93d58e5c53eb8ac6b860d9496b8c71c43b46976
Rodrigo Trece <145849765+RodrigoTrece96@users.noreply.github.com>
2023-11-12 22:17:58 +0000

    atualizando

--- meditation.py
+++ meditation.py
@@ -2,4 +2,4 @@ import random
 
 meditation = [i + 1 for i in range(20)]
 meditation = random.choice(meditation)
-print(f'Todays meditation is: {meditation}')
+print(f'Todays meditation is: day {meditation}')
