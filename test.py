9f840b54d74b9cf9bcb232a3eca7d726eab9752c
Rodrigo Trece <145849765+RodrigoTrece96@users.noreply.github.com>
2023-12-11 17:26:25 +0000

    continuar revisao de classes

--- test.py
+++ test.py
@@ -1,9 +1,41 @@
-import math
+# Rewinding classes 
 
-concetracao_proton = 0.01 #mol/L 
+# Ideia: entrar em uma loja e comprar uma pulseira 
 
-pH = -1*math.log(concetracao_proton, 10)
-pH = round(pH,2)
 
-print(pH)
+class Store:
+    def __init__(self, loja, atendente, dinheiro):
+        self.loja = loja
+        self.produtos = ["colar", "brinco", "pulseira"]
+        self.atendente = atendente
+        self.dinheiro = dinheiro
+        self.precos = {"colar":35, "brinco":20, "pulseira":25}
+        pass
+
+    def efetuando_compra(self):
+        
+
+
+        pass 
+
+class Cliente:
+    def __init__(self, nome, sexo, cidade_origem, dinheiro):
+        self.nome = nome
+        self.sexo = sexo
+        self.cidade_origem = cidade_origem
+        self.dinheiro = dinheiro 
+        pass
+
+    def comprando(self, Store):
+        pass 
+
+
+# Definindo instancias para simular 
+
+loja_1 = ("Bijus & Cia", "Raquel", 100)
+cliente_1 = ("Bon Jovi", "masculino", "New Jersey", 400)
+
+         
+ 
+
 
